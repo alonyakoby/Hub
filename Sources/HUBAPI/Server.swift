85f556bb8765f429c68121aa5ed7fd3635198ad6
alonyakoby <alon.yakoby@gmail.com>
2017-05-15 23:07:53 +0200

    IP Server

--- Sources/HUBAPI/Server.swift
+++ Sources/HUBAPI/Server.swift
@@ -37,10 +37,10 @@ public class Server: HUBAPI {
     //    static let defaultPassword = "1bed5f813a3f31c50996e3ae68ff5d7998ed45996abcd62fb5eedb9b5d930b74"
     //
     // DEVELOPMENT
-    static let defaultDBHost = "localhost"
+    static let defaultDBHost = "207.154.233.177"
     static let defaultDBPort = UInt16(5984)
     static let defaultDBName = "hubapi"
-    static let defaultUsername = "root"
+    static let defaultUsername = "alon"
     static let defaultPassword = "Passw00rd"
     
     let dbName = "hubapi"
@@ -80,8 +80,8 @@ public class Server: HUBAPI {
             Log.info("Using CF Service Credentials")
             
         } else {
-            host = "localhost"
-            username = "root"
+            host = "207.154.233.177"
+            username = "alon"
             password = "Passw00rd"
             port = UInt16(5984)
             Log.info("Using Service Development Credentials")
