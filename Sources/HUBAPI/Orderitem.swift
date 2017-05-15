ebc153c8d4261b98b7eeec8fcf5000c369376ec0
alonyakoby <alon.yakoby@gmail.com>
2017-05-15 17:26:44 +0200

    No Order

--- Sources/HUBAPI/Orderitem.swift
+++ Sources/HUBAPI/Orderitem.swift
@@ -1,22 +1,22 @@
+////
+////  Orderitem.swift
+////  HUBAPI
+////
+////  Created by Alon Yakoby on 14/05/2017.
+////
+////
 //
-//  Orderitem.swift
-//  HUBAPI
+//import Foundation
+//import SwiftyJSON
+//import LoggerAPI
+//import CouchDB
 //
-//  Created by Alon Yakoby on 14/05/2017.
-//
-//
-
-import Foundation
-import SwiftyJSON
-import LoggerAPI
-import CouchDB
-
-class Order {
-    
-    public let orderNr: String
-    public let customerName: String
-    public let
-    
-    public let createDate: String
-    public let updateDate
-}
+//class Order {
+//    
+//    public let orderNr: String
+//    public let customerName: String
+//    public let
+//    
+//    public let createDate: String
+//    public let updateDate
+//}
